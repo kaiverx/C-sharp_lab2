f78f7123537c0cfb6b617be47e71af1612e7cffa
kaiverx <praim200570@gmail.com>
2025-03-30 21:55:33 +0800

    Первая лаба гуи

---
+++ lab2_1/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.secondNum = new System.Windows.Forms.TextBox();
+            this.firstNum = new System.Windows.Forms.TextBox();
+            this.thirdNum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "первое число";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "второе число";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(117, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "третье число";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "расчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // secondNum
+            // 
+            this.secondNum.Location = new System.Drawing.Point(8, 69);
+            this.secondNum.Name = "secondNum";
+            this.secondNum.Size = new System.Drawing.Size(100, 20);
+            this.secondNum.TabIndex = 4;
+            // 
+            // firstNum
+            // 
+            this.firstNum.Location = new System.Drawing.Point(8, 16);
+            this.firstNum.Name = "firstNum";
+            this.firstNum.Size = new System.Drawing.Size(100, 20);
+            this.firstNum.TabIndex = 5;
+            // 
+            // thirdNum
+            // 
+            this.thirdNum.Location = new System.Drawing.Point(8, 124);
+            this.thirdNum.Name = "thirdNum";
+            this.thirdNum.Size = new System.Drawing.Size(100, 20);
+            this.thirdNum.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 169);
+            this.Controls.Add(this.thirdNum);
+            this.Controls.Add(this.firstNum);
+            this.Controls.Add(this.secondNum);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox secondNum;
+        private System.Windows.Forms.TextBox firstNum;
+        private System.Windows.Forms.TextBox thirdNum;
+    }
+}
+
---
+++ lab2_1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Получаем числа из текстовых полей
+                int first = int.Parse(firstNum.Text);
+                int second = int.Parse(secondNum.Text);
+                int third = int.Parse(thirdNum.Text);
+
+                // Вызываем метод сортировки
+                var (max, mid, min) = Logic.GetSorted(first, second, third);
+
+                // Выводим результат в Label
+                MessageBox.Show($"Наибольшее: {max}, Среднее: {mid}, Наименьшее: {min}");
+            }
+
+            catch (FormatException)
+            {
+                MessageBox.Show("Введите корректные целые числа!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+    public static class Logic
+    {
+        public static (int max, int mid, int min) GetSorted(int first, int second, int third)
+        {
+            int[] numbers = { first, second, third };
+            Array.Sort(numbers);
+            return (numbers[2], numbers[1], numbers[0]);
+        }
+    }
+}
